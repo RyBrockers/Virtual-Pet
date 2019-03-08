52c47afdfa32706b4472bdff84f2566391939ea0
Ryan Brockley <rybrockers@yahoo.com>
2019-03-08 19:20:40 +0000

    First test passing

--- __tests__/pet.tests.js
+++ __tests__/pet.tests.js
@@ -2,6 +2,7 @@ const Pet = require('../src/pet');
 
 describe('Pet', () => {
   it('creates and object instance', () => {
-    expect(Pet).toBeInstanceOf(Object);
+    const pet = new Pet('Benji')
+    expect(pet).toBeInstanceOf(Object);
   })
 })
