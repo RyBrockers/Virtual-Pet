9c5addd3366338c1fc502fe0374856922ffc43b7
Ryan Brockley <rybrockers@yahoo.com>
2019-03-09 08:24:23 +0000

    Added in MAXIMUM_FITNESS

--- __tests__/pet.tests.js
+++ __tests__/pet.tests.js
@@ -5,4 +5,35 @@ describe('Pet', () => {
     const pet = new Pet('Benji')
     expect(pet).toBeInstanceOf(Object);
   })
+  it('has a name property', () => {
+    const pet = new Pet('Benji');
+    expect(pet.name).toEqual('Benji');
+  })
+})
+
+describe('growUp', () => {
+  it('increases the age by 1', () => {
+    const pet = new Pet('Lucy')
+    pet.growUp();
+    expect(pet.age).toEqual(1);
+  })
+  it('increases the hunger by 5', () => {
+    const pet = new Pet('Bluey');
+    pet.growUp();
+    expect(pet.hunger).toEqual(5);
+  })
+  it('decreases the fitness by 3 ', () => {
+    const pet = new Pet('Duke');
+    pet.growUp();
+    expect(pet.fitness).toEqual(7);
+  })
+})
+
+describe('walk', () => {
+  it('increases the fitness by 4', () => {
+    const pet = new Pet('Lucy')
+    pet.walk();
+    expect(pet.fitness).toEqual(10);
+  })
+
 })
